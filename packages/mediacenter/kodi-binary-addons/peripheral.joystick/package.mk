7be3abaa6d9e338fb7f06fd47b3d736a4f545527
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-07-04 22:18:15 +0100

    kodi-binary-addons: update to latest versions

--- packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
+++ packages/mediacenter/kodi-binary-addons/peripheral.joystick/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="peripheral.joystick"
-PKG_VERSION="6764788"
+PKG_VERSION="3ae0aee"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
