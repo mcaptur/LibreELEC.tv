b6ffb20d6cc707ecb29bd7e52f96d9487aa71b64
RealJohnGalt <johngalt@fake.mail>
2017-07-12 09:11:22 -0700

    kodi: bump to db81e2c

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="6aceab2"
+PKG_VERSION="db81e2c"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
