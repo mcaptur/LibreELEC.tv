d0e7e74c2c9ff8834cbbd62c0baf38789e3cf97b
RealJohnGalt <johngalt@fake.mail>
2017-07-13 11:20:00 -0700

    kodi: bump to 7546015

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="db81e2c"
+PKG_VERSION="7546015"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
