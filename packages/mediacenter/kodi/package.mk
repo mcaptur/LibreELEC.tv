ee81ade33185703ceb3e7dfc5ec97b50b723760b
mcaptur <mark.captur@gmail.com>
2017-09-21 15:13:39 +0200

    try add kszaq changes to s912

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="2bdd690"
+PKG_VERSION="edf7549"
 PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
