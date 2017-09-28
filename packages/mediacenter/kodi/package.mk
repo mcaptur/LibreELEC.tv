8a6325d1338082e6b3dc82788fb08a7ed3d74ef5
mcaptur <mark.captur@gmail.com>
2017-09-28 04:23:25 +0200

    update kodi and add aml-vnc

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="617a394"
+PKG_VERSION="dff8c2c"
 PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
