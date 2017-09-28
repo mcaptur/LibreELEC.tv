697824abe2a2ec53581e4450c003a1cdbf73d572
mcaptur <mark.captur@gmail.com>
2017-09-28 18:54:04 +0200

    update kodi

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="dff8c2c"
+PKG_VERSION="f0f0548"
 PKG_SHA256=""
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
