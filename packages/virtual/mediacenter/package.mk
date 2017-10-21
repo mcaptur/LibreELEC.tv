f8d01b42d85d446e6564e49c9717d7747801de91
mcaptur <mark.captur@gmail.com>
2017-10-21 13:39:04 +0200

    kodi master

--- packages/virtual/mediacenter/package.mk
+++ packages/virtual/mediacenter/package.mk
@@ -28,6 +28,10 @@ PKG_SHORTDESC="Mediacenter: Metapackage"
 PKG_LONGDESC="Mediacenter: Metapackage"
 PKG_AUTORECONF="no"
 
+_add_binary_addon() {
+  [ -f $ROOT/$PACKAGES/mediacenter/kodi-binary-addons/$1/package.mk ] && PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $1" || true
+}
+
 if [ "$MEDIACENTER" = "kodi" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $MEDIACENTER-theme-$SKIN_DEFAULT"
 
@@ -42,9 +46,47 @@ if [ "$MEDIACENTER" = "kodi" ]; then
 # other packages
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET LibreELEC-settings \
                                           xmlstarlet"
+  
 
+  _ADDONS_PVR=yes
+  _ADDONS_ADSP=no
+#  _ADDONS_GAME=no
+  _ADDONS_OTHER=yes
+
+  # various PVR clients
+    if [ "${_ADDONS_PVR}" = "yes" ]; then
+#    _add_binary_addon "pvr.argustv"
+#    _add_binary_addon "pvr.demo"
+#    _add_binary_addon "pvr.dvblink"
+#    _add_binary_addon "pvr.dvbviewer"
+#    _add_binary_addon "pvr.filmon"
+    _add_binary_addon "pvr.hts"
+#    _add_binary_addon "pvr.hdhomerun"
+    _add_binary_addon "pvr.iptvsimple"
+#    _add_binary_addon "pvr.mediaportal.tvserver"
+#    _add_binary_addon "pvr.mythtv"
+#    _add_binary_addon "pvr.nextpvr"
+#    _add_binary_addon "pvr.njoy"
+#    _add_binary_addon "pvr.octonet"
+#    _add_binary_addon "pvr.pctv"
+#    _add_binary_addon "pvr.stalker"
+#    _add_binary_addon "pvr.teleboy"
+#    _add_binary_addon "pvr.vbox"
+    _add_binary_addon "pvr.vdr.vnsi"
+#    _add_binary_addon "pvr.vuplus"
+#    _add_binary_addon "pvr.wmc"
+#    _add_binary_addon "pvr.zattoo"
+  fi
+ 
   if [ "$JOYSTICK_SUPPORT" = "yes" ]; then
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET peripheral.joystick"
   fi
 
+  if [ "${_ADDONS_OTHER}" = "yes" ]; then
+    _add_binary_addon "inputstream.adaptive"
+    _add_binary_addon "inputstream.smoothstream"
+    _add_binary_addon "inputstream.rtmp"
+    _add_binary_addon "imagedecoder.raw"
+    _add_binary_addon "vfs.rar"
+  fi
 fi
